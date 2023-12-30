3ad17073bb5fbcd1687062989a73b60c5d47acae
Inaxo <Oliwier_glowala@tlen.pl>
2023-12-30 09:57:21 +0100

    Add payment-success blade content

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/checkout/{productID}', [\Inaxo\LaravelStripeHandler\controllers\StripePaymentController::class, 'checkout'])->name('checkout');
+Route::post('/checkout/{productID}', [\Inaxo\LaravelStripeHandler\controllers\StripePaymentController::class, 'checkout'])->name('checkout');
 Route::get('/payment-success', [\Inaxo\LaravelStripeHandler\controllers\StripePaymentController::class, 'success'])->name('payment-success');
 Route::get('/payment-cancel', [\Inaxo\LaravelStripeHandler\controllers\StripePaymentController::class, 'cancel'])->name('payment-cancel');
 Route::get('/getProducts', [\Inaxo\LaravelStripeHandler\controllers\StripePaymentController::class, 'getProductFromXMLFile'])->name('getProducts');
